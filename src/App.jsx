49a6c261b1e1076fcec19f4560b5f66bac824ddf
Fabio <tahiantsoaFabio17@gmail.com>
2025-12-03 13:32:10 +0300

    Remove Helmet to fix blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { useLocation } from 'react-router-dom'
-import { Helmet } from 'react-helmet-async'
 import Home from './pages/Home'
 import About from './pages/About'
 import ProjectsPage from './pages/ProjectsPage'
@@ -12,30 +11,18 @@ import Footer from './components/Footer'
 export default function App(){
   const location = useLocation()
   return (
-    <>
-      <Helmet>
-        <title>Fabio Rakotoharimanga - Portfolio</title>
-        <meta name="description" content="Portfolio de Fabio Rakotoharimanga, développeur web spécialisé en React et Django." />
-        <meta name="author" content="Fabio Rakotoharimanga" />
-        <meta property="og:title" content="Fabio Rakotoharimanga - Portfolio" />
-        <meta property="og:description" content="Portfolio développeur web React Django." />
-        <meta property="og:url" content="https://portfolio-fabio-inky.vercel.app" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <link rel="canonical" href="https://portfolio-fabio-inky.vercel.app" />
-      </Helmet>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-              <Route path="/" element={<Home/>} />
-              <Route path="/about" element={<About/>} />
-              <Route path="/projects" element={<ProjectsPage/>} />
-            </Routes>
-          </AnimatePresence>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/projects" element={<ProjectsPage/>} />
+          </Routes>
+        </AnimatePresence>
+      </main>
+      <Footer />
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App/>} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
