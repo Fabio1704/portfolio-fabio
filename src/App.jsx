b521a223050a8158370dbfd3a3951a4eb58f5511
Fabio <tahiantsoaFabio17@gmail.com>
2025-12-03 13:36:02 +0300

    Remove AnimatePresence to fix rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import { useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import ProjectsPage from './pages/ProjectsPage'
@@ -9,18 +7,15 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export default function App(){
-  const location = useLocation()
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1">
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home/>} />
-            <Route path="/about" element={<About/>} />
-            <Route path="/projects" element={<ProjectsPage/>} />
-          </Routes>
-        </AnimatePresence>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<ProjectsPage/>} />
+        </Routes>
       </main>
       <Footer />
     </div>
